0bdbaa7173ce46dce3b4ab103933d8176c14240f
Manishankar Balasubramanian <manishankar.b@Shankar.local>
2018-08-17 17:42:51 +0530

    guice test updated

--- src/test/java/ResourceTest/AuthorResourceTest.java
+++ src/test/java/ResourceTest/AuthorResourceTest.java
@@ -2,40 +2,66 @@ package ResourceTest;
 
 
 import api.Author;
+import com.squarespace.jersey2.guice.JerseyGuiceUtils;
 import dao.AuthorDAO;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.*;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import resource.AuthorResource;
+import service.AuthorServiceImpl;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.*;
 
+@RunWith(MockitoJUnitRunner.class)
 public class AuthorResourceTest {
 
-    private static final AuthorDAO dao = mock(AuthorDAO.class);
+    //private static final AuthorDAO dao = mock(AuthorDAO.class);
+
+
+    static {
+        JerseyGuiceUtils.install((s, serviceLocator) -> null);
+    }
+
+    @InjectMocks
+    private static AuthorServiceImpl authorServiceImpl = mock(AuthorServiceImpl.class);
+
+    //@Mock
+    //private AuthorService authorService = mock(AuthorService.class);
+
+    @Mock
+    private static AuthorDAO authorDAO = mock(AuthorDAO.class);
 
     @ClassRule
+
     public static ResourceTestRule resources = ResourceTestRule.builder()
-            .addResource(new AuthorResource(dao))
+            .addResource(new AuthorResource(authorServiceImpl))
             .build();
 
     private final Author author = new Author(null,1,"Shankar");
 
+
     @Before
     public void setup() {
-
-        when(dao.findById(eq(1))).thenReturn(author);
+        MockitoAnnotations.initMocks(this);
+        //when(authorServiceImpl.findById(eq(1))).thenReturn(author);
+        doReturn(author).when(authorServiceImpl).findById(anyInt());
+        //doReturn(author).when(authorDAO).findById(anyInt());
     }
 
     @Test
     public void getAuthorTest() {
-        System.out.println(resources);System.out.println(dao);
+        System.out.println(resources);
         Author responseAuthor = resources.target("/author/001").request().get(Author.class);
         assertThat(responseAuthor)
                 .isEqualTo(author);
         System.out.println("\nExpected : "+ author.toString());
         System.out.println("Actual : "+ responseAuthor.toString());
-        verify(dao).findById(1);
+        verify(authorServiceImpl).findById(1);
     }
 
 }
--- src/test/java/ResourceTest/BookResourceTest.java
+++ src/test/java/ResourceTest/BookResourceTest.java
@@ -2,17 +2,20 @@ package ResourceTest;
 
 import api.Author;
 import api.Book;
-import dao.AuthorDAO;
-import dao.BookDAO;
+import com.squarespace.jersey2.guice.JerseyGuiceUtils;
 import io.dropwizard.testing.junit.ResourceTestRule;
 import org.junit.Before;
 import org.junit.ClassRule;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.MockitoAnnotations;
+import org.mockito.runners.MockitoJUnitRunner;
 import resource.BookResource;
-
+import service.AuthorServiceImpl;
+import service.BookServiceImpl;
 import java.util.ArrayList;
 import java.util.List;
-
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Matchers.anyInt;
 import static org.mockito.Matchers.eq;
@@ -20,13 +23,25 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
+@RunWith(MockitoJUnitRunner.class)
 public class BookResourceTest {
-    private static final BookDAO bkdao = mock(BookDAO.class);
-    private static final AuthorDAO audao = mock(AuthorDAO.class);
+//    private static final BookDAO bkdao = mock(BookDAO.class);
+//    private static final AuthorDAO audao = mock(AuthorDAO.class);
+
+    static {
+        JerseyGuiceUtils.install((s, serviceLocator) -> null);
+    }
+
+    @InjectMocks
+    private static final AuthorServiceImpl authorServiceImpl = mock(AuthorServiceImpl.class);
+
+    @InjectMocks
+    private static final BookServiceImpl bookServiceImpl = mock(BookServiceImpl.class);
 
     @ClassRule
+
     public static ResourceTestRule resources = ResourceTestRule.builder()
-            .addResource(new BookResource(bkdao,audao))
+            .addResource(new BookResource(authorServiceImpl,bookServiceImpl))
             .build();
 
     private final Author author = new Author(null,1,"shankar");
@@ -35,12 +50,13 @@ public class BookResourceTest {
     private List<Book> bookList = new ArrayList<>();
     @Before
     public void setup() {
+        MockitoAnnotations.initMocks(this);
         bookList.add(book1);
         bookList.add(book2);
 
-        when(bkdao.getAll()).thenReturn(bookList);
-        when(bkdao.getBookByAuthId(eq(author))).thenReturn(bookList);
-        when(audao.findById(anyInt())).thenReturn(author);
+        when(bookServiceImpl.getAll()).thenReturn(bookList);
+        when(bookServiceImpl.getBookByAuthId(eq(author))).thenReturn(bookList);
+        when(authorServiceImpl.findById(anyInt())).thenReturn(author);
     }
 
     @Test
@@ -51,7 +67,7 @@ public class BookResourceTest {
         System.out.println("Actual : "+ capturedList.toString());
         assertThat(capturedList.toString())
                 .isEqualTo(expected);
-        verify(bkdao).getAll();
+        verify(bookServiceImpl).getAll();
     }
 
     @Test
@@ -63,7 +79,7 @@ public class BookResourceTest {
         System.out.println("Actual : "+ capturedList.toString());
         assertThat(capturedList.toString())
                 .isEqualTo(expected);
-        verify(bkdao).getBookByAuthId(author);
+        verify(bookServiceImpl).getBookByAuthId(author);
     }
 
 }
